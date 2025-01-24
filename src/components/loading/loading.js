5946dede2ff2c04ac0da1c289a98f35834e13c2d
Ivan Lebedev <iamil50113@gmail.com>
2025-01-25 02:49:18 +0300

    asdv11

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,13 +1,15 @@
 import styles from './loading.module.css'
 import "./loading.css"
 import image from './image.webp'
+import image2 from './image.png'
 import Image from "next/image";
 import Closer from "@/components/loading/closer/closer";
 
 export default function Loading() {
     return <div id="loading">
         <div id={styles.div}>
-            <Image src={image} alt="Loading Image"/>
+            <Image src={image} alt="Loading Image" id={styles.img1}/>
+            <Image src={image2} alt="Loading Image" id={styles.img2}/>
         </div>
         <Closer/>
     </div>
