a557195282aefc5118b670fc029fd4ef63021798
Ivan Lebedev <iamil50113@gmail.com>
2025-01-25 04:18:36 +0300

    asdv105

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,15 +1,14 @@
 import styles from './loading.module.css'
 import "./loading.css"
-import image from './image.webp'
-import image2 from './image.png'
-import Image from "next/image";
 import Closer from "@/components/loading/closer/closer";
 
 export default function Loading() {
     return <div id="loading">
         <div id={styles.div}>
-            <Image src={image} alt="Loading Image" id={styles.img1}/>
-            <Image src={image2} alt="Loading Image" id={styles.img2}/>
+            <video loop height="100%" autoPlay muted id="video">
+                <source src="/backvideo.mp4"
+                        type='video/mp4'/>
+            </video>
         </div>
         <Closer/>
     </div>
