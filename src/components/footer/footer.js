4ce59bf9c0dad05889a4d3655910b80d987bd01d
Ivan Lebedev <iamil50113@gmail.com>
2025-02-04 02:37:33 +0300

    logotypes refactoring

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
-import enLogo from './enLogo.png'
-import ruLogo from './ruLogo.png'
+import ruLogo from  "./ruLogo.svg"
+import enLogo from  "./enLogo.svg"
 import styles from './footer.module.css'
 import "./footer.css"
 import Navigation from "@/components/navigation2/navigation";
@@ -8,7 +8,7 @@ import Navigation from "@/components/navigation2/navigation";
 export default function Footer({enLan}) {
     return <footer>
         <div id={styles.footer}>
-            <div id={styles.logo}><Image src={(enLan === true) ? enLogo : ruLogo} alt="logo"/></div>
+            <div id={styles.logo}><Image src={enLan ? enLogo : ruLogo} alt="footerLogo" /></div>
             <a href="mailto:logistica@povpro.ru">
                 logistica@povpro.ru
             </a>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,15 @@
-import Image from 'next/image'
-import ruLogo from './ruLogo.png'
-import enLogo from './enLogo.png'
 import styles from './header.module.css'
 import Link from "next/link";
 import Navigation from "../navigation/navigation";
 import "./header.css"
 import MobileMenu from "@/components/mobileMenu/menu";
+import Logo from "@/components/logo/logo";
 
 export default function Header({enLan}) {
     return <header id={styles.header} className="base_grid">
-        <div id={styles.logo}><Image src={(enLan === true) ? enLogo : ruLogo} alt="logo"/></div>
+        <div id={styles.logo}>
+            <Logo enLan={enLan}/>
+        </div>
         <Navigation enLan={enLan}/>
         <Link id={styles.lan_btn} href={(enLan === true) ? "/" : "/en"}>
             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
